c1c18384ac1f7b41a59e8d7fca116825cd6cc3ec
Meli Cordero <melisacorderoarias@hotmail.com>
2017-10-21 22:43:42 -0600

    Add files via upload

--- SP BD_PLANTAS/tardias_periodo_tiempo.sql
+++ SP BD_PLANTAS/tardias_periodo_tiempo.sql
@@ -1,14 +1,16 @@
 DELIMITER $$
 CREATE PROCEDURE tardias_periodo_tiempo(IN FechaInicio date, IN FechaFin date)
 BEGIN
-SELECT m.id_empleado, m.fecha, m.marca_entrada, he.hora_entrada
+SELECT m.id_empleado, count(*)
 FROM marcas m
 inner join horarios_empleados he
 on he.id_empleado = m.id_empleado
 	and nombreDia(m.fecha) = he.nombre
 where m.marca_entrada > he.hora_entrada
-	and m.fecha between FechaInicio and FechaFin;
+	and m.fecha between FechaInicio and FechaFin
+group by m.id_empleado;
 END$$
 DELIMITER ;
 
 
+CALL `bd_planta`.`tardias_periodo_tiempo`('2017-01-01','2017-8-31');
