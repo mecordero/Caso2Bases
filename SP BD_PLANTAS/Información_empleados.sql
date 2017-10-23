dd5e3636c27c52f25e85caae243e5a644f9e85bd
fabriales14 <fabriales14@gmail.com>
2017-10-23 13:56:09 -0600

    Cambios a consultas
    Se le quito coalesce porque las fechas no aceptan 0 se pueden dejar en NULL no afecta el resultado

---
+++ SP BD_PLANTAS/Información_empleados.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Información_empleado`(IN id_empleado int)
+BEGIN
+	SELECT e.id_empleado as 'Id empleado' , e.id_planta as 'Planta',  e.nombre as 'Nombre', e.apellidos as 'Apellidos',  e.fecha_ingreso 'Ingreso',
+    e.fecha_salida as 'Fecha Salida', a1.nombre as 'Supervisor',   d.nombre as 'Departamento' , te.nombre 'Tipo de empleado'
+    FROM empleado e
+    join empleado a1 on e.id_supervisor = a1.id_empleado
+    join departamento d on d.id_departamento = e.id_departamento
+	join tipo_empleado te on te.id_tipo = e.id_tipo 
+
+    WHERE e.id_empleado = id_empleado;
+END
