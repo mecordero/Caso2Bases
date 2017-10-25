70d6a2aca24dd5227331f4eac13dd537944b2fa4
Meli <melissamadri@gmail.com>
2017-10-24 20:40:24 -0600

    Add files via upload
    Da la informacion de los empleados , segun el tipo de calendario seleccionado

---
+++ SP BD_PLANTAS/seleccion_calendario.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `seleccion_calendario`(IN tipo_calendario INT)
+BEGIN
+	SELECT e.id_empleado, e.id_supervisor,e.id_tipo,e.id_planilla,
+    p.salario_hora,p.salario_hora_extra,p.cantidad_horas_semanal,p.salario_bruto,
+    p.salario_neto
+    FROM empleado e
+    JOIN planilla p on e.id_planilla = p.id_planilla
+    JOIN calendario c on p.id_calendario = c.id_calendario
+    WHERE c.id_calendario = tipo_calendario
+	;
+END
