80e255489d4be6cb7109353bef215e145860185a
fabriales14 <fabriales14@gmail.com>
2017-10-24 23:17:51 -0600

    Delete empleados_sin_salida.sql

--- SP BD_PLANTAS/empleados_sin_salida.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE empleados_sin_salida(IN FechaInicio date, IN FechaFin date)
-BEGIN
-SELECT m.id_empleado, m.fecha, m.marca_salida
-FROM marcas m
-where m.marca_salida is NULL
-	and m.fecha between FechaInicio and FechaFin;
-END$$
-DELIMITER ;
